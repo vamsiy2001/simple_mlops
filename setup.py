cf33982bbda48363bac2f339e0be38fb7fa1691a
Vamsi <vamsiy2001@gmail.com>
2023-03-10 12:19:18 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="src",
+    description="It's a wine quality package",
+    version= "0.0.1",
+    author="vamsi",
+    packages=find_packages(),
+    license="MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,7 @@
+
+
+
+def test_generic():
+    a = 'sd'
+    b = 'sd'
+    assert a == b # when pytest is run this gets checked
